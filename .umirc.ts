4c648cb3f28696a828e32011e1331cd240bd2a2f
TinsFox <fox@tinsfox.com>
2021-07-02 14:58:56 +0800

    add a post

--- .umirc.ts
+++ .umirc.ts
@@ -4,25 +4,32 @@ export default defineConfig({
   title: 'TinsFox',
   mode: 'site',
   // logo:'/images/logo.jpg',
-  locales:[['zh-CN', '中文'],['en-US', 'English']],
+  locales: [
+    ['zh-CN', '中文'],
+    ['en-US', 'English'],
+  ],
   fastRefresh: {},
   base: './',
   publicPath: './',
-  hash:true,
+  hash: true,
   exportStatic: {}, // 将所有路由输出为 HTML 目录结构，以免刷新页面时 404
   navs: [
     null, // null 值代表保留约定式生成的导航，只做增量配置
     {
-      title: '小工具',
-      path: '/tools',
+      title: '更多',
+      path: '/more',
       children: [
-        { title: '提问的艺术', path: 'https://github.com/ryanhanwu/How-To-Ask-Questions-The-Smart-Way/blob/main/README-zh_CN.md' },
+        {
+          title: '提问的艺术',
+          path: 'https://github.com/ryanhanwu/How-To-Ask-Questions-The-Smart-Way/blob/main/README-zh_CN.md',
+        },
+        {
+          title: 'GitHub',
+          path: 'https://github.com/TinsFox/TinsFox.github.io',
+        },
         { title: 'dumi', path: 'https://d.umijs.org/config' },
       ],
-    },{
-      title:"GitHub",
-      path:"https://github.com/TinsFox/TinsFox.github.io"
-    }
+    },
   ],
   // more config: https://d.umijs.org/config
 });
